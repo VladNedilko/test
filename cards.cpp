acae56f738b26160ec238c01628fe796dc69728b
Vlad Nedilko <52749366+VladNedilko@users.noreply.github.com>
2022-11-14 19:00:51 +0200

    Add files via upload

---
+++ cards.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int N,M;
+    cout<<"Введіть масть карти 1-4";
+    cin>>M;
+    cout<<"Введіть цифру від 1-14";
+    cin>>N;
+    switch(M){
+        case 1:cout<<"Піки";break;
+        case 2:cout<<"Трефи";break;
+        case 3:cout<<"Бубни";break;
+        case 4:cout<<"Черви";break;
+        default:cout<<"Input correct number";
+    }
+    switch(N){
+        case 6:cout<<"Шістка";break;
+        case 7:cout<<"Сімка";break;
+        case 8:cout<<"Вісімка";break;
+        case 9:cout<<"Девятка";break;
+        case 10:cout<<"Десятка";break;
+        case 11:cout<<"Валет";break;
+        case 12:cout<<"Дама";break;
+        default:cout<<"Input correct number";
+        cout<<endl;
+    }
+    
+    return 0;
+}
